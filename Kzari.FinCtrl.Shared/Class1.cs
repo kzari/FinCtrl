b075ff65f30e98d7af36145f7edad51d4e395276
Julio Cazari <julio.cazari@quiver.local>
2018-07-03 14:38:22 -0300

    Organizando a solution

---
+++ Kzari.FinCtrl.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Kzari.FinCtrl.Shared
+{
+    public class Class1
+    {
+    }
+}
---
+++ Kzari.FinCtrl.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Kzari.FinCtrl.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
